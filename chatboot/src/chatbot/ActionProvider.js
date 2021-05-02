02e75964bed00ee27f9afb9d6493aa95681be02e
chiheb-sayah <57415231+chiheb-sayah@users.noreply.github.com>
2021-05-03 00:00:24 +0100

    go

---
+++ chatboot/src/chatbot/ActionProvider.js
@@ -0,0 +1,64 @@
+class ActionProvider {
+  constructor(createChatBotMessage, setStateFunc, createClientMessage) {
+    this.createChatBotMessage = createChatBotMessage;
+    this.setState = setStateFunc;
+    this.createClientMessage = createClientMessage;
+  }
+
+  handleMessageParser = () => {
+    const messages = this.createChatBotMessage(
+      "The message parser controls how the bot reads input and decides which action to invoke.",
+      { widget: "messageParser", withAvatar: true }
+    );
+
+    this.addMessageToBotState(messages);
+  };
+  handleGreeting = () => {
+    const messages = this.createChatBotMessage(" What's your ID", {
+      withAvatar: true,
+    });
+    this.addMessageToBotState(messages);
+  };
+  handleQuiz = () => {
+    const messages = this.createChatBotMessage(
+      "what is the specialty to look for?",
+      {
+        widget: "Sp",
+        withAvatar: true,
+      }
+    );
+    this.addMessageToBotState(messages);
+  };
+
+  handleDefault = () => {
+    const message = this.createChatBotMessage("Ask me Again", {
+      withAvatar: true,
+    });
+    this.addMessageToBotState(message);
+  };
+  handlemusic = () => {
+    const message = this.createChatBotMessage(
+      "Take a break and listen to music",
+      {
+        widget: "music",
+        withAvatar: true,
+      }
+    );
+    this.addMessageToBotState(message);
+  };
+  addMessageToBotState = (messages) => {
+    if (Array.isArray(messages)) {
+      this.setState((state) => ({
+        ...state,
+        messages: [...state.messages, ...messages],
+      }));
+    } else {
+      this.setState((state) => ({
+        ...state,
+        messages: [...state.messages, messages],
+      }));
+    }
+  };
+}
+
+export default ActionProvider;
